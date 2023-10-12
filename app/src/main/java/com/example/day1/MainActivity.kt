247385d330519e161ac350dd35a96e1b66c90c78
dCOMMICS <sanwurld@gmail.com>
2023-10-12 23:04:26 +0300

    welcome

--- app/src/main/java/com/example/day1/MainActivity.kt
+++ app/src/main/java/com/example/day1/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Welcome on board Mr. $name!",
         modifier = modifier
     )
 }
